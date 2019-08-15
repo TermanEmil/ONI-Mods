f627d4dbbcc5268ce73613ff1476ae2ff3a9b4ee
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-08-15 15:45:40 -0400

    [Mass Based Digging] Create mod.  This one was far easier than expected.

---
+++ src/MassBasedDigging/MassBasedDiggingPatches.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+namespace MassBasedDigging
+{
+    [HarmonyPatch(typeof(Workable), nameof(Workable.GetEfficiencyMultiplier))]
+    internal class MassBasedDiggingPatches
+    {
+        public static void Postfix(ref Workable __instance, ref float __result)
+        {
+            __result *= 1800 / Grid.Mass[Grid.PosToCell(__instance)];
+        }
+    }
+}
