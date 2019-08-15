46e69dee5a8d4890f20fce44f27d143b38e16a25
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-08-15 17:17:56 -0400

    [Show Area In Box Tool] Add mod.  This mod shows the total area as well as the dimensions in drag tools.

---
+++ src/ShowAreaInBoxTool/ShowAreaPatches.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+
+namespace ShowAreaInBoxTool
+{
+    [HarmonyPatch(typeof(DragTool), nameof(DragTool.OnMouseMove))]
+    public class ShowAreaPatches
+    {
+        public static string AreaFormat = "{0} x {1} : {2}";
+
+        public static void Postfix(ref DragTool __instance)
+        {
+            if ((DragTool.Mode) Traverse.Create(__instance).Method("GetMode").GetValue() != DragTool.Mode.Box) return;
+
+            var sizeVector = ((SpriteRenderer) AccessTools.Field(typeof(DragTool), "areaVisualizerSpriteRenderer")
+                .GetValue(__instance)).size;
+            var visualizerText = AccessTools.Field(typeof(DragTool), "areaVisualizerText");
+            if ((Guid) visualizerText.GetValue(__instance) == Guid.Empty) return;
+            var roundedSizeVector =
+                new Vector2I(Mathf.RoundToInt(sizeVector.x), Mathf.RoundToInt(sizeVector.y));
+            var text = NameDisplayScreen.Instance.GetWorldText((Guid) visualizerText.GetValue(__instance))
+                .GetComponent<LocText>();
+            text.text = string.Format(AreaFormat, roundedSizeVector.x, roundedSizeVector.y,
+                roundedSizeVector.x * roundedSizeVector.y);
+        }
+    }
+}
