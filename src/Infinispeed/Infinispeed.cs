cb30933c7976cc7750be1ae4bcdd3e89a4a11f51
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-08-16 09:01:07 -0400

    [Infinispeed] Fix eating chores being too fast and eating a lot more food than normal

--- src/Infinispeed/Infinispeed.cs
+++ src/Infinispeed/Infinispeed.cs
@@ -9,8 +9,9 @@ namespace Infinispeed
     [HarmonyPatch(typeof(Workable), nameof(Workable.GetEfficiencyMultiplier))]
     internal class Infinispeed
     {
-        public static void Postfix(ref float __result)
+        public static void Postfix(ref Workable __instance, ref float __result)
         {
+            if (__instance is Edible) return;
             __result = float.PositiveInfinity;
         }
     }
