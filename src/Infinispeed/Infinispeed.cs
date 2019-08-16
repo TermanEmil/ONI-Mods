493315674a88118d86ec5625aeaa91b1440d2cac
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-08-15 21:49:44 -0400

    [Infinispeed] Add Infinispeed mod

---
+++ src/Infinispeed/Infinispeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+
+namespace Infinispeed
+{
+    [HarmonyPatch(typeof(Workable), nameof(Workable.GetEfficiencyMultiplier))]
+    internal class Infinispeed
+    {
+        public static void Postfix(ref float __result)
+        {
+            __result = float.PositiveInfinity;
+        }
+    }
+}
