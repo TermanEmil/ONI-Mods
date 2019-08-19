3dbbb6cce6eeef332c94430b713fc27ccb312391
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-08-19 15:45:04 -0400

    [All Joules] Add All Joules mod.
    This mod makes all heat and energy values represented in terms of Joules instead of DTU or Watts

---
+++ src/AllJoules/UnitNamePatches.cs
@@ -0,0 +1,18 @@
+﻿using Units = STRINGS.UI.UNITSUFFIXES;
+
+namespace AllJoules
+{
+    internal class UnitNamePatches
+    {
+        public static void OnLoad()
+        {
+            Units.ELECTRICAL.WATT = Units.ELECTRICAL.JOULE + "/s";
+            Units.ELECTRICAL.KILOWATT = Units.ELECTRICAL.KILOJOULE + "/s";
+
+            Units.HEAT.DTU = Units.ELECTRICAL.JOULE;
+            Units.HEAT.KDTU = Units.ELECTRICAL.KILOJOULE;
+            Units.HEAT.DTU_S = Units.ELECTRICAL.JOULE + "/s";
+            Units.HEAT.KDTU_S = Units.ELECTRICAL.KILOJOULE + "/s";
+        }
+    }
+}
