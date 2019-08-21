2c59734ba1f7adf7df8de1181ff49811befc080a
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-08-20 21:09:39 -0400

    [Diode] Get it working functionally

--- src/Diode/DiodeConfig.cs
+++ src/Diode/DiodeConfig.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using TUNING;
+﻿using TUNING;
 using UnityEngine;
 
 namespace Diode
@@ -7,21 +6,23 @@ namespace Diode
     internal class DiodeConfig : IBuildingConfig
     {
         public const string Id = "Diode";
+        public const string DisplayName = "Diode";
+        public const string Description = "Diodes separate circuits and transfer power in one direction";
+        public const string Effect = "TODO";
         private readonly string _anim = "valvegas_logic_kanim";
 
         public override BuildingDef CreateBuildingDef()
         {
             var hitpoints = 30;
             var constructionTime = 10f;
-            var constructionMass = BUILDINGS.CONSTRUCTION_MASS_KG.TIER1;
-            var refinedMetals = MATERIALS.REFINED_METALS;
+            var constructionMaterials = MATERIALS.REFINED_METALS;
             var meltingPoint = 1600f;
             var buildLocationRule = BuildLocationRule.Anywhere;
-            var noise = NOISE_POLLUTION.NOISY.TIER1;
             var buildingDef = BuildingTemplates.CreateBuildingDef(Id, 1, 2, _anim, hitpoints, constructionTime,
-                constructionMass, refinedMetals, meltingPoint, buildLocationRule, BUILDINGS.DECOR.PENALTY.TIER0, noise);
+                BUILDINGS.CONSTRUCTION_MASS_KG.TIER1, constructionMaterials, meltingPoint, buildLocationRule, BUILDINGS.DECOR.PENALTY.TIER0, NOISE_POLLUTION.NOISY.TIER1);
             buildingDef.Floodable = false;
             buildingDef.RequiresPowerInput = true;
+            buildingDef.GeneratorWattageRating = float.PositiveInfinity;
             buildingDef.PowerInputOffset = CellOffset.none;
             buildingDef.PowerOutputOffset = new CellOffset(1, 0);
             buildingDef.ViewMode = OverlayModes.Power.ID;
@@ -30,9 +31,17 @@ namespace Diode
             return buildingDef;
         }
 
+        public override void ConfigureBuildingTemplate(GameObject go, Tag prefabTag)
+        {
+            var battery = go.AddOrGet<Battery>();
+            battery.powerSortOrder = 1000;
+            battery.capacity = float.PositiveInfinity;
+            battery.chargeWattage = float.PositiveInfinity;
+            go.AddOrGet<PowerTransformer>().powerDistributionOrder = 9;
+        }
+
         public override void DoPostConfigureComplete(GameObject go)
         {
-            go.AddOrGet<Diode>();
         }
     }
 }
--- src/Diode/DiodePatches.cs
+++ src/Diode/DiodePatches.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Diagnostics;
+using CaiLib.Utils;
+using Harmony;
+using static CaiLib.Utils.BuildingUtils;
+using static CaiLib.Utils.StringUtils;
 
 namespace Diode
 {
     class DiodePatches
     {
-        //TODO: Add building
+        [HarmonyPatch(typeof(Debug), "Assert", typeof(bool))]
+        internal static class Debug_Assert_Patch
+        {
+            internal static void Prefix(bool condition)
+            {
+                if (condition) return;
+                var stack = new StackTrace();
+                if (stack.FrameCount <= 2) return;
+                var callName = stack.GetFrame(2).GetMethod().FullDescription();
+                Debug.LogWarning(
+                    $"[AssertFinder] The following method called an Assert that is about to fail:\n{callName}");
+            }
+        }
+
+        [HarmonyPatch(typeof(GeneratedBuildings))]
+        [HarmonyPatch(nameof(GeneratedBuildings.LoadGeneratedBuildings))]
+        public class GeneratedBuildings_LoadGeneratedBuildings_Patch
+        {
+            public static void Prefix()
+            {
+                AddBuildingStrings(DiodeConfig.Id, DiodeConfig.DisplayName,
+                    DiodeConfig.Description, DiodeConfig.Effect);
+                AddBuildingToPlanScreen(GameStrings.PlanMenuCategory.Plumbing, DiodeConfig.Id);
+            }
+        }
     }
 }
