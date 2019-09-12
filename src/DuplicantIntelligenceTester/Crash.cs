b4699ea3d0f3c0e52154bcec901cb10a5c78556b
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-09-12 14:38:46 -0400

    lots o testing stuff

---
+++ src/DuplicantIntelligenceTester/Crash.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Harmony;
+using KMod;
+using Steamworks;
+
+namespace DuplicantIntelligenceTester
+{
+    [HarmonyPatch(typeof(Steam), "UpdateMods")]
+    public class Crash
+    {
+        public static void Postfix(IEnumerable<PublishedFileId_t> removed)
+        {
+            if(removed.)
+            Sim.SIM_DebugCrash();
+        }
+    }
+}
