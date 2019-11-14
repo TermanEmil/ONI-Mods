1d65b915818918c5c5f4a82fbd6559191d7a40bb
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-11-14 07:58:48 -0500

    [EdibleDuplicants] Add Butcher Table

---
+++ src/EdibleDuplicants/ButcherTableConfig.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace EdibleDuplicants
+{
+    class ButcherTableConfig : IBuildingConfig
+    {
+        public const string Id = "ButcherTable";
+        public static string DisplayName = "Butcher Table";
+        public static string Description = "Butchers the corpses of dead duplicants." +
+            "  Duplicants that perform \"work\" here will definitely not have fun, but sometimes, you need to eat.";
+
+        public override BuildingDef CreateBuildingDef()
+        {
+            var buildingDef = BuildingTemplates.CreateBuildingDef(Id, 3, 2, "clothingfactory_kanim", 100, 120, new[] { 100f }, TUNING.MATERIALS.ALL_METALS, 473.15f, BuildLocationRule.OnFloor, TUNING.DECOR.PENALTY.TIER2, TUNING.NOISE_POLLUTION.NOISY.TIER0);
+            buildingDef.AudioCategory = "Metal";
+            return buildingDef;
+        }
+
+        public override void DoPostConfigureComplete(GameObject go)
+        {
+            var storage = go.AddOrGet<Storage>();
+            storage.showInUI = false;
+        }
+    }
+}
---
+++ src/EdibleDuplicants/EdibleDuplicantsPatches.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CaiLib;
+
+namespace EdibleDuplicants
+{
+    class EdibleDuplicantsPatches
+    {
+        public static void OnLoad()
+        {
+            CaiLib.Utils.BuildingUtils.AddBuildingToPlanScreen(CaiLib.Utils.GameStrings.PlanMenuCategory.Food, ButcherTableConfig.Id);
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.BUTCHERTABLE.NAME", ButcherTableConfig.DisplayName);
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.BUTCHERTABLE.EFFECT", "EFFECT");
+            Strings.Add("STRINGS.BUILDINGS.PREFABS.BUTCHERTABLE.DESC", ButcherTableConfig.Description);
+        }
+    }
+}
