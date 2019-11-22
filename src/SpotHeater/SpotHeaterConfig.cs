1b7fd26a7c1edf82e304659aaec1a9381bf103d0
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-11-22 11:06:40 -0500

    [SpotHeater] Add Spot Heater building

---
+++ src/SpotHeater/SpotHeaterConfig.cs
@@ -0,0 +1,42 @@
+﻿using Steamworks;
+using STRINGS;
+using TUNING;
+using UnityEngine;
+using BUILDINGS = TUNING.BUILDINGS;
+
+namespace SpotHeater
+{
+    public class SpotHeaterConfig : IBuildingConfig
+    {
+        public const string Id = "asquared31415.SpotHeater";
+        public const string DisplayName = "Spot Heater";
+        public const string Description = "A spot heater is useful for keeping things just a tad bit warmer.";
+        public static readonly string Effect = "Radiates a small amount of." + UI.FormatAsLink("Heat", "HEAT");
+
+        public override BuildingDef CreateBuildingDef()
+        {
+            var buildingDef = BuildingTemplates.CreateBuildingDef(Id, 1, 1, "farmtilerotating_kanim",
+                BUILDINGS.HITPOINTS.TIER2, BUILDINGS.CONSTRUCTION_TIME_SECONDS.TIER2,
+                BUILDINGS.CONSTRUCTION_MASS_KG.TIER3, MATERIALS.ALL_METALS, BUILDINGS.MELTING_POINT_KELVIN.TIER4,
+                BuildLocationRule.Tile, BUILDINGS.DECOR.PENALTY.TIER0, NOISE_POLLUTION.NOISY.TIER1);
+            buildingDef.RequiresPowerInput = true;
+            buildingDef.EnergyConsumptionWhenActive = 60;
+            buildingDef.SelfHeatKilowattsWhenActive = 1.5f;
+            buildingDef.PowerInputOffset = CellOffset.none;
+            buildingDef.ViewMode = OverlayModes.Power.ID;
+            buildingDef.AudioCategory = "HollowMetal";
+            buildingDef.OverheatTemperature = BUILDINGS.OVERHEAT_TEMPERATURES.HIGH_2;
+            return buildingDef;
+        }
+
+        public override void ConfigureBuildingTemplate(GameObject go, Tag prefabTag)
+        {
+            go.AddOrGet<SpaceHeater>().targetTemperature = 323.15f;
+        }
+
+        public override void DoPostConfigureComplete(GameObject go)
+        {
+            go.AddOrGetDef<PoweredActiveController.Def>();
+        }
+    }
+}
---
+++ src/SpotHeater/SpotHeaterPatches.cs
@@ -0,0 +1,16 @@
+﻿using CaiLib.Utils;
+
+namespace SpotHeater
+{
+    public class SpotHeaterPatches
+    {
+        public static void OnLoad()
+        {
+            BuildingUtils.AddBuildingToPlanScreen(GameStrings.PlanMenuCategory.Utilities, SpotHeaterConfig.Id);
+            BuildingUtils.AddBuildingToTechnology(GameStrings.Technology.Gases.TemperatureModulation,
+                SpotHeaterConfig.Id);
+            StringUtils.AddBuildingStrings(SpotHeaterConfig.Id, SpotHeaterConfig.DisplayName,
+                SpotHeaterConfig.Description, SpotHeaterConfig.Effect);
+        }
+    }
+}
