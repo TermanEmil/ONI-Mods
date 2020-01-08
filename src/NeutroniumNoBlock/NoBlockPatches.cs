0f894465cfd461ccecc0a06b645ca6abfbf00506
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-01-07 19:58:05 -0500

    [UnblockableRockets] Add unblockable rockets mod

---
+++ src/NeutroniumNoBlock/NoBlockPatches.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+
+namespace UnblockableRockets
+{
+    [HarmonyPatch(typeof(ConditionFlightPathIsClear), "CanReachSpace")]
+    public class NoBlockPatches
+    {
+        public static void Postfix(ref bool __result, ref ConditionFlightPathIsClear __instance)
+        {
+            Traverse.Create(__instance).Field("obstructedTile").SetValue(default(int));
+            __result = true;
+        }
+    }
+}
