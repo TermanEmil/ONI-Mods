60aa2c97fdff2e53aa6b4942ce018e2416efa358
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-01-29 14:46:18 -0500

    [MicroTransformer] Create micro transformer
    Still needs anim to be adjusted.  Position/anchor?

---
+++ AssetsSource/MicroTransformer/build.bat
@@ -0,0 +1,2 @@
+..\kanimal-cli.exe kanim -iso ../../Assets/MicroTransformer/anim/assets/micro_transformer micro_transformer.scml
+pause
---
+++ src/MicroTransformer/SmallTransformerPatches.cs
@@ -0,0 +1,15 @@
+﻿using CaiLib.Utils;
+
+namespace MicroTransformer
+{
+    public class SmallTransformerPatches
+    {
+        public static void OnLoad()
+        {
+            BuildingUtils.AddBuildingToPlanScreen(GameStrings.PlanMenuCategory.Power, SmallTransformerConfig.Id);
+            BuildingUtils.AddBuildingToTechnology(GameStrings.Technology.Food.Ranching, SmallTransformerConfig.Id);
+            StringUtils.AddBuildingStrings(SmallTransformerConfig.Id, SmallTransformerConfig.DisplayName,
+                SmallTransformerConfig.Description, SmallTransformerConfig.Effect);
+        }
+    }
+}
