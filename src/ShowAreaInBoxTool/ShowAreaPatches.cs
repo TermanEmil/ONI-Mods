7bf3c3ad20a79d5425261012d1403325ce268c19
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-03-24 14:38:17 -0400

    DragToolArea - Show area under tool info
    Bug: shows on all info cards, and doesn't reset in some cases

--- src/ShowAreaInBoxTool/ShowAreaPatches.cs
+++ src/ShowAreaInBoxTool/ShowAreaPatches.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using Harmony;
 using UnityEngine;
@@ -13,27 +14,68 @@ namespace ShowAreaInBoxTool
             Debug.Log(
                 $"[ShowAreaInBoxTool] Loading mod version {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}");
         }
-    }
 
-    [HarmonyPatch(typeof(DragTool), nameof(DragTool.OnMouseMove))]
-    public class ShowAreaPatches
-    {
-        public static string AreaFormat = "{0} x {1} : {2}";
-
-        public static void Postfix(ref DragTool __instance)
+        public static void PostPatch(HarmonyInstance harmonyInstance)
         {
-            if ((DragTool.Mode) Traverse.Create(__instance).Method("GetMode").GetValue() != DragTool.Mode.Box) return;
+            var postfix = typeof(ModOnLoad).GetMethod("DrawerPostfix");
+            var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes())
+                .Where(type => type.IsSubclassOf(typeof(HoverTextConfiguration)));
+            foreach (var hoverType in types)
+            {
+                var methodInfo = AccessTools.Method(hoverType, "UpdateHoverElements");
+                harmonyInstance.Patch(methodInfo, null, new HarmonyMethod(postfix));
+            }
+        }
 
-            var sizeVector = ((SpriteRenderer) AccessTools.Field(typeof(DragTool), "areaVisualizerSpriteRenderer")
-                .GetValue(__instance)).size;
-            var visualizerText = AccessTools.Field(typeof(DragTool), "areaVisualizerText");
-            if ((Guid) visualizerText.GetValue(__instance) == Guid.Empty) return;
-            var roundedSizeVector =
-                new Vector2I(Mathf.RoundToInt(sizeVector.x), Mathf.RoundToInt(sizeVector.y));
-            var text = NameDisplayScreen.Instance.GetWorldText((Guid) visualizerText.GetValue(__instance))
-                .GetComponent<LocText>();
-            text.text = string.Format(AreaFormat, roundedSizeVector.x, roundedSizeVector.y,
+        public static void DrawerPostfix(HoverTextConfiguration __instance)
+        {
+            var tool = ToolMenu.Instance.activeTool;
+            var roundedSizeVector = Vector2I.zero;
+            if (tool != null && tool.GetType().IsSubclassOf(typeof(DragTool)))
+            {
+                if ((DragTool.Mode) Traverse.Create(tool).Method("GetMode").GetValue() == DragTool.Mode.Box)
+                {
+                    if (((DragTool) tool).Dragging)
+                    {
+                        var sizeVector = ((SpriteRenderer) AccessTools
+                            .Field(typeof(DragTool), "areaVisualizerSpriteRenderer")
+                            .GetValue(tool)).size;
+                        roundedSizeVector = new Vector2I(Mathf.RoundToInt(sizeVector.x), Mathf.RoundToInt(sizeVector.y));
+                    }
+                }
+            }
+            
+            const string areaFormat = "Selection Size: {0} x {1} : {2} cells";
+            var text = string.Format(areaFormat, roundedSizeVector.x, roundedSizeVector.y,
                 roundedSizeVector.x * roundedSizeVector.y);
+            var drawer = HoverTextScreen.Instance.drawer;
+            drawer.BeginShadowBar();
+            drawer.DrawText(text, __instance.Styles_Title.Standard);
+            drawer.EndShadowBar();
+            drawer.EndDrawing();
+        }
+
+        [HarmonyPatch(typeof(DragTool), "OnMouseMove")]
+        public class ShowAreaPatches
+        {
+            private const string AreaFormat = "{0} x {1} : {2}";
+
+            public static void Postfix(ref DragTool __instance)
+            {
+                if ((DragTool.Mode) Traverse.Create(__instance).Method("GetMode").GetValue() !=
+                    DragTool.Mode.Box) return;
+
+                var sizeVector = ((SpriteRenderer) AccessTools.Field(typeof(DragTool), "areaVisualizerSpriteRenderer")
+                    .GetValue(__instance)).size;
+                var visualizerText = AccessTools.Field(typeof(DragTool), "areaVisualizerText");
+                if ((Guid) visualizerText.GetValue(__instance) == Guid.Empty) return;
+                var roundedSizeVector =
+                    new Vector2I(Mathf.RoundToInt(sizeVector.x), Mathf.RoundToInt(sizeVector.y));
+                var text = NameDisplayScreen.Instance.GetWorldText((Guid) visualizerText.GetValue(__instance))
+                    .GetComponent<LocText>();
+                text.text = string.Format(AreaFormat, roundedSizeVector.x, roundedSizeVector.y,
+                    roundedSizeVector.x * roundedSizeVector.y);
+            }
         }
     }
 }
