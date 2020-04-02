ebfa59778d83e948595337a3bb59c6a37cbff70b
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-04-02 16:27:16 -0400

    Clothing Locker Mod

---
+++ src/ClothingLocker/ClothingLockerPatches.cs
@@ -0,0 +1,18 @@
+﻿using CaiLib.Utils;
+
+namespace ClothingLocker
+{
+    public class ClothingLockerPatches
+    {
+        public static void OnLoad()
+        {
+            BuildingUtils.AddBuildingToPlanScreen( GameStrings.PlanMenuCategory.Base, ClothingLockerConfig.Id );
+            StringUtils.AddBuildingStrings(
+                ClothingLockerConfig.Id,
+                ClothingLockerConfig.Name,
+                ClothingLockerConfig.Desc,
+                ClothingLockerConfig.Effect
+            );
+        }
+    }
+}
