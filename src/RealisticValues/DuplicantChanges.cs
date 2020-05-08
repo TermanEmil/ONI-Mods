4e8fcadabd7f51d2ae2cc08f8bcc666abd215f6e
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-05-08 08:19:37 -0400

    Better breathing patches

--- src/RealisticValues/DuplicantChanges.cs
+++ src/RealisticValues/DuplicantChanges.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using Harmony;
+using Klei.AI;
 using UnityEngine;
 
 namespace RealisticValues
@@ -10,8 +11,8 @@ namespace RealisticValues
     public static class DuplicantChanges
     {
         public const float O2Conversion = 0.25f;
-        public const float MinCo2 = 0.050f;
-        public const float MinO2 = 0.150f;
+        public const float MinCo2       = 0.100f;
+        public const float MinO2        = 0.150f;
 
         [HarmonyPatch(typeof(MinionConfig), "CreatePrefab")]
         public class MinionBreathPatch
@@ -24,49 +25,108 @@ namespace RealisticValues
         }
 
         [HarmonyPatch(typeof(OxygenBreather), nameof(OxygenBreather.Sim200ms))]
-        public static class OxygenBreather_Sim200ms_Patches
+        public static class BreatherPatches
         {
+            public static float AccumulatedO2;
+
             public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> orig)
             {
-                var codes = orig.ToList();
+                List<CodeInstruction> codes = orig.ToList();
                 var shouldEmit = AccessTools.Method(typeof(OxygenBreather.IGasProvider), "ShouldEmitCO2");
                 for(var i = 0; i < codes.Count; ++i)
                 {
-                    if((MethodInfo)codes[i].operand == shouldEmit)
+                    if(codes[i].opcode == OpCodes.Callvirt && codes[i].operand is MethodInfo m && m == shouldEmit)
                     {
                         // Needs to properly accumulate and emit
                         // Probably pass to helper
-                        var idx = i + 3;
+                        var idx = i + 2;
+                        // inst
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldarg_0));
+                        // mass
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldloc_0));
+                        // temp
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldarg_0));
+                        var temp = AccessTools.Field(typeof(OxygenBreather), "temperature");
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldfld, temp));
+                        var val = AccessTools.Field(typeof(AmountInstance), "value");
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldfld, val));
+                        var accumulate = AccessTools.Method(
+                            typeof(BreatherPatches),
+                            nameof(BreatherPatches.AccumulateO2)
+                        );
+
+                        codes.Insert(idx, new CodeInstruction(OpCodes.Call, accumulate));
+
+                        idx = codes.Count - 1;
+                        // inst
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldarg_0));
+                        // hasAir
                         codes.Insert(idx++, new CodeInstruction(OpCodes.Dup));
+                        var hasAir = AccessTools.Field(typeof(OxygenBreather), "hasAir");
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldfld, hasAir));
+                        // temp
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldarg_0));
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldfld, temp));
+                        codes.Insert(idx++, new CodeInstruction(OpCodes.Ldfld, val));
+                        var dumpAll = AccessTools.Method(typeof(BreatherPatches), nameof(BreatherPatches.DumpAll));
+                        codes.Insert(idx, new CodeInstruction(OpCodes.Call, dumpAll));
+
+                        return codes;
                     }
                 }
+
+                Debug.Log("something wrong");
+                return codes;
             }
 
-            public static void SpawnO2()
+            private static void DumpAll(OxygenBreather inst, bool hasAir, float temp)
             {
-                
-            }
-        }
+                if(hasAir)
+                    return;
 
-        [HarmonyPatch(typeof(CO2Manager), "SpawnBreath")]
-        public class BreathModifier
-        {
-            public static void Postfix(CO2Manager __instance, Vector3 position, float mass, float temperature)
-            {
-                var o2Mass = mass * (1 / O2Conversion) * (1 - O2Conversion);
-                Debug.Log($"CO2 Mass: {mass}");
-                Debug.Log($"Breath mass: {o2Mass}");
+                var position = inst.transform.GetPosition();
+                position.x += inst.GetComponent<Facing>().GetFacing() ? -inst.mouthOffset.x : inst.mouthOffset.x;
+                position.y += inst.mouthOffset.y;
+                position.z -= 0.5f;
                 var cell = Grid.CellAbove(Grid.PosToCell(position));
-                //Debug.Log($"{temperature} -> {outTemp}");
+                Debug.Log($"Producing {DuplicantChanges.MinO2}");
                 SimMessages.AddRemoveSubstance(
                     cell,
                     SimHashes.Oxygen,
                     CellEventLogger.Instance.OxygenModifierSimUpdate,
-                    mass,
-                    temperature,
+                    AccumulatedO2,
+                    temp,
                     byte.MaxValue,
                     0
                 );
+
+                AccumulatedO2 = 0;
+            }
+
+            private static void AccumulateO2(OxygenBreather inst, float mass, float temp)
+            {
+                var toRecycle = mass * (1 - DuplicantChanges.O2Conversion);
+                Debug.Log($"Mass: {mass} Recycle: {toRecycle}");
+                AccumulatedO2 += toRecycle;
+                if(AccumulatedO2 >= DuplicantChanges.MinO2)
+                {
+                    AccumulatedO2 -= DuplicantChanges.MinO2;
+                    var position = inst.transform.GetPosition();
+                    position.x += inst.GetComponent<Facing>().GetFacing() ? -inst.mouthOffset.x : inst.mouthOffset.x;
+                    position.y += inst.mouthOffset.y;
+                    position.z -= 0.5f;
+                    var cell = Grid.CellAbove(Grid.PosToCell(position));
+                    Debug.Log($"Producing {DuplicantChanges.MinO2}");
+                    SimMessages.AddRemoveSubstance(
+                        cell,
+                        SimHashes.Oxygen,
+                        CellEventLogger.Instance.OxygenModifierSimUpdate,
+                        DuplicantChanges.MinO2,
+                        temp,
+                        byte.MaxValue,
+                        0
+                    );
+                }
             }
         }
 
