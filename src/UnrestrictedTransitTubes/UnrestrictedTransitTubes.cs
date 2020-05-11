e43d64e92964b48f649c7b968e88ac583ecbbf19
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-05-11 12:17:03 -0400

    Unrestricted Tubes

---
+++ src/UnrestrictedTransitTubes/UnrestrictedTransitTubes.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Harmony;
+
+namespace UnrestrictedTransitTubes
+{
+    public class Hooks
+    {
+        public static void OnLoad()
+        {
+            Debug.Log(
+                $"[UnrestrictedTransitTubes] Loading mod version {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}"
+            );
+        }
+    }
+
+    [HarmonyPatch(typeof(UtilityNetworkTubesManager), nameof(UtilityNetworkTubesManager.CanAddConnection))]
+    public class Patch
+    {
+        public static void Postfix(ref bool __result) { __result = true; }
+    }
+}
